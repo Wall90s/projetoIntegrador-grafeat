d18bf72470503b8ad92173600bd440afc637c992
Giyo Santana Dias <giosantdias@gmail.com>
2023-03-17 13:38:45 -0300

    Primeiro arquivo

---
+++ src/main/java/org/generation/grafeat/GrafeatApplication.java
@@ -0,0 +1,13 @@
+package org.generation.grafeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrafeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/generation/grafeat/GrafeatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.grafeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
